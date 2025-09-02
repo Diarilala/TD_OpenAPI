283098b1fcd3e9feae44b9da0caf082c94ce29a1
Diarilala <hei.diarilala@gmail.com>
2025-09-02 16:39:01 +0300

    Feat: add exo1 and exo1.yaml

---
+++ exo.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
+
+app = FastAPI()
+
+@app.get("/ping")
+def ping():
+    return Response(content="pong", status_code=200, media_type="text/plain")
